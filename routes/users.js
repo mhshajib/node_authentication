70b67ded54d4124e8ba4d829a74f461c90f468f2
mhshajib <shajib0513@gmail.com>
2017-08-02 18:09:07 +0600

    Initial upload without updating README

---
+++ routes/users.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('../models/user');
+
+//Get Register
+router.get('/register', function (req, res) {
+    res.render('register');
+});
+
+//User Register
+router.post('/register', function (req, res) {
+    var name = req.body.name;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var confirm_password = req.body.confirm_password;
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('username', 'User is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email not valid').isEmail();
+    req.checkBody('password', 'Passord required').notEmpty();
+    req.checkBody('confirm_password', 'Password mismatch').equals(req.body.password);
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.render('register', {
+            errors: errors
+        });
+    }else{
+        var newUser = new User({
+            name: name,
+            username: username,
+            email: email,
+            password: password
+        });
+        User.getUserByUsername(username, function (err, user) {
+            if(err) throw err;
+            if(user){
+                req.flash('error_msg', 'User Already Exist');
+                res.redirect('/users/register');
+            }else{
+                User.createUser(newUser, function (err, user) {
+                    if(err) throw err;
+                    console.log(user);
+                });
+
+                req.flash('success_msg', 'You are registered and can now login');
+
+                res.redirect('/users/login');
+            }
+        });
+    }
+});
+
+//Get Login
+router.get('/login', ensureAuthenticated, function (req, res) {
+    res.render('login');
+});
+
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.getUserByUsername(username, function (err, user) {
+            if(err) throw err;
+            if(!user){
+                return done(null, false, {message: 'Unknown User'});
+            }
+            User.comparePassword(password, user.password, function (err, isMatch) {
+                if(err) throw err;
+                if(isMatch){
+                    return done(null, user);
+                }else{
+                    return done(null, false, {message: 'Invalid Password'});
+                }
+            });
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+//User Login
+router.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/users/login', failureFlash: true}), function(req, res) {
+    res.redirect('/');
+});
+
+//User Logout
+router.get('/logout', function (req, res, next) {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
+
+function ensureAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        res.redirect('/');
+    }else{
+        return next();
+    }
+}
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressHandlebars = require('express-handlebars');
+var expressValidator = require('express-validator');
+var connectFlash = require('connect-flash');
+var expressSession = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/passport', { useMongoClient: true});
+
+//Init app
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', expressHandlebars({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+
+//Set statuc folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Express Session
+app.use(expressSession({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave:true
+}));
+
+//Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express validator
+app.use(expressValidator({
+	errorFormatter: function (param, msg, value) {
+		var namespace = param.split('.')
+			, root = namespace.shift()
+			, formParam = root;
+		while(namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
+
+//Connect flash
+app.use(connectFlash());
+
+//Global vars
+app.use(function (req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	res.locals.user = req.user || null;
+	res.locals.host = 'http://localhost:'+app.get('port');
+	next();
+});
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+//Set Port
+app.set('port',3300);
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongodb is not running.');
+    process.exit();
+}).on('connected', function() {
+    app.listen(app.get('port'), function() {
+        console.log('Server started at : http://localhost:' + app.get('port'));
+    });
+});
